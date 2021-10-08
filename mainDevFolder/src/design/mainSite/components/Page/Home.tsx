25fded6ff6341a01b74081264347730e5d6aa559
Kenneth  Cunningham <kennethcunningham@Kenneths-MacBook-Air.local>
2021-10-08 03:14:40 -0400

    commit

--- mainDevFolder/src/design/mainSite/components/Page/Home.tsx
+++ mainDevFolder/src/design/mainSite/components/Page/Home.tsx
@@ -7,22 +7,23 @@ import * as React from 'react';
     const Home:React.FC = ()=>{
     const [old,newOld] = React.useState([])
 
-       React.useEffect(() =>{
-            (async () =>{
-                let newData = await fetch('http://localhost:4000/client_suite')
-                let reply = await newData.json();
-                newOld(reply)
-            })()
-       },[])
-       old.map((i)=>{
-           console.log(i.first_name)
-       })
+    //    React.useEffect(() =>{
+    //         (async () =>{
+    //             let newData = await fetch('http://localhost:4000/client_suite')
+    //             let reply = await newData.json();
+    //             newOld(reply)
+    //         })()
+    //    },[])
+    //    old.map((i)=>{
+    //        console.log(i.first_name)
+    //    })
         
         return (
            <React.Fragment>
-               {old.map((i,j)=>( 
+               {/* {old.map((i,j)=>( 
                    <p key={j}>{i.first_name}</p>
-               ))}
+               ))} */}
+               <p>Hello to all</p>
            </React.Fragment>
         )
     };
