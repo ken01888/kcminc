9bd31ae15373c1c2a801c59edc7513f9e30a0e22
Kenneth  Cunningham <kennethcunningham@Kenneths-iMac.local>
2021-11-02 00:47:48 -0400

    complete nutritional analysis section

--- mainDevFolder/src/design/mainSite/components/Page/Home.tsx
+++ mainDevFolder/src/design/mainSite/components/Page/Home.tsx
@@ -15,7 +15,8 @@ import {
   Card,
   Tag,
   Descriptions,
-  Badge
+  Badge,
+  Popover
 } from 'antd'
 import {
   LeftSquareOutlined,
@@ -30,7 +31,7 @@ import {
 } from '@ant-design/icons'
 import { motion } from 'framer-motion'
 import { duration } from 'moment'
-import { FcOrgUnit, FcGenealogy } from 'react-icons/fc'
+import { FcOrgUnit, FcGenealogy, FcGlobe } from 'react-icons/fc'
 const { Panel } = Collapse
 const Home: React.FC = () => {
   const [old, newOld] = React.useState([])
@@ -38,11 +39,13 @@ const Home: React.FC = () => {
   React.useEffect(() => {
     (async () => {
       let newData = await fetch('http://localhost:4000')
-      let reply:{foods:any[]} = await newData.json()
-      let newReply = await reply.foods[1].foodNutrients.map(i=> i.nutrientId)
-      console.log(newReply)
-      
-   
+      let reply: { foods: any[] } = await newData.json()
+      let newReply: [] = await reply.foods[1].foodNutrients
+
+      newReply.map((i) => {
+        console.log(i)
+      })
+
     })()
   }, [])
   const data = [
@@ -62,136 +65,50 @@ const Home: React.FC = () => {
   return (
     <React.Fragment>
       <Row justify='center' className='heroSection' gutter={[24, 64]}>
-        <Col xs={12}>
+        <Col xs={16}>
           <h1 className='h1_heroSection'>
             {' '}
             Solutions for Food Support Institutions.
           </h1>
         </Col>
 
-        <Col offset={1} xs={18}>
+        <Col offset={1} xs={16}>
           <h2>How we help </h2>
           <p>
-            At <span>KCM</span> <sup>INC</sup> we strive to provide our clients
-            with innovative solutions that benefit both people and the
-            planet.Our solutions protect the longevity of your organization, its
-            competitive position and improves stakeholder value.
+
+
+            We help add value to your business through our suite of
+            innovative business solutions developed to improve the well-being
+            of the general public and optimize commercial enterprises.
+
+
           </p>
         </Col>
       </Row>
+
 
       {/* {Solutions Section} */}
       <Row justify='center'>
         <Col offset={1} xs={18} className='heroSection'>
-          <h2 className='h2Solutions'>Our Solutions</h2>
+          <h2 className='h2Solutions'>Responsible Solutions</h2>
           <p>
-            Changes in consumer demands and environmental needs command that
-            institutions remain adaptive. Our solutions help optimize your
-            organization's performance and simplify the development of nutrient
-            rich products.
+            Our business solutions help change the way business
+            interact with their consumers.
           </p>
 
-          <Collapse
-            activeKey={1}
-            expandIcon={({ isActive }) => (
-              <CaretRightOutlined
-                rotate={isActive ? 90 : 0}
-                style={{ color: '#009688' }}
-
-              />
-            )}
-          >
-            <Panel header='Present Solutions' key='1'>
-              <Collapse>
-                <Panel
-                  header={<Tag color='#009688'>Nutritional Analysis</Tag>}
-                  key='2'
-                >
-                  <p>
-                    Our nutritional analysis solution gives food manufactures a
-                    complete nutritional breakdown of their products based on
-                    ingredient weight and serving size. This solution provides a
-                    nutritional graph very similar to those found on the back of
-                    household nondurable goods.
-                  </p>
-
-                  <Descriptions title="Product Info" layout="horizontal" >
-                    <Descriptions.Item label="Product">Chick-fil-a Chicken Sandwich</Descriptions.Item>
-                    <Descriptions.Item label="Serving Size">183g</Descriptions.Item>
-                    <Descriptions.Item label="Test">Nutritional Content</Descriptions.Item>
-                    <Descriptions.Item label="Calories">440</Descriptions.Item>
-                    <Descriptions.Item label="FAT (G)">
-                      17
-                    </Descriptions.Item>
-                    <Descriptions.Item label="SAT. FAT (G)">
-                      4
-                    </Descriptions.Item>
-                    <Descriptions.Item label="TRANS FAT (G)">0</Descriptions.Item>
-                    <Descriptions.Item label="CHOLESTEROL (MG)">70</Descriptions.Item>
-                    <Descriptions.Item label="SODIUM (MG)">1400</Descriptions.Item>
-                    <Descriptions.Item label="CARBOHYDRATES(G)">41</Descriptions.Item>
-                    <Descriptions.Item label="FIBER">1</Descriptions.Item>
-                    <Descriptions.Item label="SUGAR (G)">6</Descriptions.Item>
-                    <Descriptions.Item label="PROTEIN">29</Descriptions.Item>
-
-
-
-                    <Descriptions.Item label="Source" span={2}>
-                      <a href="https://www.chick-fil-a.com/nutrition-allergens" target='_blank'>Chick-fil-a</a>
-                    </Descriptions.Item>
-
-                    <Descriptions.Item label="Information">
-                      <a href="https://www.chick-fil-a.com/nutrition-allergens" target='_blank'>Chick-fil-a</a>
-                    </Descriptions.Item>
-                  </Descriptions>
-                  
-
-                  <Collapse ghost>
-                    <Panel header={'Deep Analysis'} key='2'>
-                      
-                      <Descriptions title={`Chick-fil-a Chicken Sandwich`} layout="horizontal" >
-                    <Descriptions.Item label="Product">{`Chick-fil-a Chicken Sandwich`}</Descriptions.Item>
-                    <Descriptions.Item label="Serving Size">183g</Descriptions.Item>
-                    <Descriptions.Item label="Test">Nutritional Content</Descriptions.Item>
-                    <Descriptions.Item label="Calories">440</Descriptions.Item>
-                    <Descriptions.Item label="FAT (G)">
-                      17
-                    </Descriptions.Item>
-                    <Descriptions.Item label="SAT. FAT (G)">
-                      4
-                    </Descriptions.Item>
-                    <Descriptions.Item label="TRANS FAT (G)">0</Descriptions.Item>
-                    <Descriptions.Item label="CHOLESTEROL (MG)">70</Descriptions.Item>
-                    <Descriptions.Item label="SODIUM (MG)">1400</Descriptions.Item>
-                    <Descriptions.Item label="CARBOHYDRATES(G)">41</Descriptions.Item>
-                    <Descriptions.Item label="FIBER">1</Descriptions.Item>
-                    <Descriptions.Item label="SUGAR (G)">6</Descriptions.Item>
-                    <Descriptions.Item label="PROTEIN">29</Descriptions.Item>
-
-
-
-                    <Descriptions.Item label="Source" span={2}>
-                      <a href="https://www.chick-fil-a.com/nutrition-allergens" target='_blank'>Chick-fil-a</a>
-                    </Descriptions.Item>
-
-                    <Descriptions.Item label="Information">
-                      <a href="https://www.chick-fil-a.com/nutrition-allergens" target='_blank'>Chick-fil-a</a>
-                    </Descriptions.Item>
-                  </Descriptions>
-                    </Panel>
-                  </Collapse>
-                </Panel>
-              </Collapse>
-            </Panel>
-          </Collapse>
+         
+        
         </Col>
       </Row>
 
-      {/* <Row justify='center'>
-        <Col md={18} className='contactSection'>
-          <h1>Contact</h1>
+      <Row style={{marginTop:'1rem'}}justify='center'>
+        <Col offset={1} xs={18}>
+          <h1>Nutritional Analysis</h1>
+
         </Col>
-      </Row> */}
+      </Row>
+      
+
     </React.Fragment>
   )
 }
