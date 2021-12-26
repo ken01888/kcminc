f08bcb100b8bc437a380875fe2e99637040007ee
Kenneth  Cunningham <kennethcunningham@Kenneths-MacBook-Air.local>
2021-12-26 00:46:13 -0500

    complete landing page tomorrow

--- mainDevFolder/src/App.tsx
+++ mainDevFolder/src/App.tsx
@@ -1,27 +1,44 @@
 import * as React from 'react'
-import {  Layout,Menu,Anchor,Row,Col, PageHeader, Button, Drawer} from 'antd'
+import {
+  Layout,
+  Menu,
+  Anchor,
+  Row,
+  Col,
+  PageHeader,
+  Button,
+  Drawer
+} from 'antd'
 import Home from './design/mainSite/components/Page/Home'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Health from './design/mainSite/components/Page/Health'
 import Contact from './design/mainSite/components/Page/Contact'
-import {AppstoreOutlined, BulbOutlined, FacebookFilled,FolderViewOutlined,HomeOutlined,MailOutlined,MessageOutlined,QuestionOutlined,TwitterSquareFilled} from '@ant-design/icons'
+import {
+  AppstoreOutlined,
+  BulbOutlined,
+  FacebookFilled,
+  FolderViewOutlined,
+  HomeOutlined,
+  MailOutlined,
+  MessageOutlined,
+  QuestionOutlined,
+  TwitterSquareFilled
+} from '@ant-design/icons'
 import Nutrition from './design/mainSite/components/Page/Nutrition'
 import Nutrition2 from './design/mainSite/components/Page/Nutrition2'
 import Nutrition3 from './design/mainSite/components/Page/Nutrition3'
-
 
 const { Header, Content, Footer } = Layout
 const { Link } = Anchor
 
 const App = () => {
-  const [visible, setVisible] = React.useState(false);
+  const [visible, setVisible] = React.useState(false)
   const showDrawer = () => {
-    setVisible(true);
-  };
+    setVisible(true)
+  }
   const onClose = () => {
-    setVisible(false);
-  };
-  
+    setVisible(false)
+  }
 
   return (
     <Layout>
@@ -33,43 +50,71 @@ const App = () => {
           background: '#fafafa'
         }}
       >
-   <Menu  mode="horizontal" className='largeDisplay' >
-        <Menu.Item key="home" icon={<HomeOutlined style={{color:'#009688'}} />}>
-          KCM INC
-        </Menu.Item>
-        <Menu.Item key="solutions"  icon={<BulbOutlined style={{color:'#009688'}} />}>
-         Solutions
-        </Menu.Item>
-        <Menu.Item key="faq"  icon={<QuestionOutlined style={{color:'#009688'}} />}>
-        FAQ
-        </Menu.Item>
-        <Menu.Item key="contact"  icon={<MessageOutlined style={{color:'#009688'}} />}>
-         Contact us
-        </Menu.Item>
-      </Menu>
-      <Menu  mode="horizontal" className='smallDisplay' >
-        <Menu.Item key="home" icon={<HomeOutlined />}>
-          KCM INC
-        </Menu.Item>
-        <Menu.Item key="solutions">
-        <Button type="dashed" onClick={showDrawer} icon={<FolderViewOutlined style={{fontSize:'1.25rem',color:'#009688'}}  />} >
-        
-      </Button>
-        </Menu.Item>
-       
-      </Menu>
+        <Menu mode='horizontal' className='largeDisplay'>
+          <Menu.Item
+            key='home'
+            icon={<HomeOutlined style={{ color: '#009688' }} />}
+          >
+            KCM INC
+          </Menu.Item>
+          <Menu.Item
+            key='solutions'
+            icon={<BulbOutlined style={{ color: '#009688' }} />}
+          >
+            Solutions
+          </Menu.Item>
+          <Menu.Item
+            key='faq'
+            icon={<QuestionOutlined style={{ color: '#009688' }} />}
+          >
+            FAQ
+          </Menu.Item>
+          <Menu.Item
+            key='contact'
+            icon={<MessageOutlined style={{ color: '#009688' }} />}
+          >
+            Contact Us
+          </Menu.Item>
+        </Menu>
+        <Menu mode='horizontal' className='smallDisplay'>
+          <Menu.Item key='home' icon={<HomeOutlined />}>
+            KCM INC
+          </Menu.Item>
+          <Menu.Item key='solutions'>
+            <Button
+              type='dashed'
+              onClick={showDrawer}
+              icon={
+                <FolderViewOutlined
+                  style={{ fontSize: '1.25rem', color: '#009688' }}
+                />
+              }
+            ></Button>
+          </Menu.Item>
+        </Menu>
       </Header>
       <Content className='site-layout'>
-      <Drawer title="Basic Drawer" placement="left" onClose={onClose} visible={visible}>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-      </Drawer>
+        <Drawer
+          title='Basic Drawer'
+          placement='left'
+          onClose={onClose}
+          visible={visible}
+        >
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+        </Drawer>
         <Router>
           <Switch>
             <Route exact path='/' component={Home} />
-            <Route path='/health/nutritionalanalysis/3' component={Nutrition3} />
-            <Route path='/health/nutritionalanalysis/2/:client' component={Nutrition2} />
+            <Route
+              path='/health/nutritionalanalysis/3'
+              component={Nutrition3}
+            />
+            <Route
+              path='/health/nutritionalanalysis/2/:client'
+              component={Nutrition2}
+            />
             <Route path='/health/nutritionalanalysis' component={Nutrition} />
             <Route path='/health' component={Health} />
             <Route path='/contact' component={Contact} />
--- mainDevFolder/src/design/mainSite/components/Page/Home.tsx
+++ mainDevFolder/src/design/mainSite/components/Page/Home.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import { Parallax } from 'rc-scroll-anim'
-import { Row, Col, Select, Input, Button, Card, Tag } from 'antd'
+import { Row, Col, Select, Input, Button, Card, Tag, Divider } from 'antd'
 import {
   AreaChartOutlined,
   ArrowLeftOutlined,
@@ -10,6 +10,7 @@ import {
   CheckCircleOutlined,
   DollarCircleOutlined,
   ExperimentFilled,
+  GlobalOutlined,
   PhoneOutlined,
   PieChartOutlined,
   TwitterOutlined
@@ -44,28 +45,42 @@ const Home: React.FC = () => {
 
   return (
     <React.Fragment>
-      <Row justify='space-around' className='heroSection' >
-        <Col xs={18} >
-          <h1 className='h1Hero'>Responsibily designed business solutions.</h1>
+      <Row justify='center' gutter={[16, 32]}>
+        <Col xs={14} className='heroSection'>
+          <h1>High impact business solutions.</h1>
           <p>
-            <span style={{ color: '#009688', fontWeight: 600 }}>KCM INC</span>{' '}
-            provides integrative technological solutions to institutions
-            operating within a broad set of industries. Our solutions are
-            designed to improve the well-being of both national and global
-            communities.
+            Solutions designed to improve the well-being of consumers and
+            institutions.
           </p>
-          <Link to='/health'>
+          <GlobalOutlined
+            style={{
+              color: '#009688',
+              fontSize: '7rem',
+              textAlign: 'center',
+              marginBottom: '5rem'
+            }}
+          />
+          {/* <Link to='/health'>
             <Button type="primary" shape="round" icon={<ExperimentFilled />} size='large' htmlType='button'>
               Nutrimap
             </Button>
-          </Link>
+          </Link> */}
+          {/* <div className='heroDiv'>
+            <motion.p>Health</motion.p>
+            <motion.p>Security</motion.p>
+            <motion.p>Shelter</motion.p>
+          </div> */}
         </Col>
       </Row>
+      <Row justify='center'>
+        <Col className='section2' xs={24} md={14} >
+          <h1>Assisiting a broad range of industries.</h1>
+          <p>We provide integrative technological solutions to institutions operating within a broad range of industries. Our solutions are designed to improve the well-being of global communinties and institutions.</p>
 
-     
-
-
-
+        </Col>
+       
+      </Row>
+      <Divider className='divider'>Solutions</Divider>
 
     </React.Fragment>
   )
