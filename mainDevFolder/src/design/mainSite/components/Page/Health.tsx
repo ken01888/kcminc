b21aad1d800df1015e528d2630d4d038296cb9ed
Kenneth  Cunningham <kennethcunningham@Kenneths-iMac.local>
2021-12-31 09:48:24 -0500

    commit

--- mainDevFolder/src/design/mainSite/components/Page/Health.tsx
+++ mainDevFolder/src/design/mainSite/components/Page/Health.tsx
@@ -1,5 +1,5 @@
 import { CheckCircleOutlined, ExperimentFilled } from '@ant-design/icons';
-import { Row, Col, Button } from 'antd';
+import { Row, Col, Button, Form, Input, Checkbox, Card } from 'antd';
 import * as React from 'react';
 import { RiHealthBookFill } from 'react-icons/Ri';
 import { Link, useHistory } from 'react-router-dom'
@@ -21,13 +21,21 @@ const Health = () => {
         history.push('/health/nutritionalanalysis')
     }
 
+    const onFinish = (values: any) => {
+        console.log('Success:', values);
+    };
+
+    const onFinishFailed = (errorInfo: any) => {
+        console.log('Failed:', errorInfo);
+    };
+
 
 
     return (
-        <Row style={{ height: '90vh', display: 'flex', alignItems: 'center', justifyContent: 'center', backgroundColor: '#fafafa' }}>
+        <Row style={{ height: '90vh', display: 'flex', alignItems: 'center',backgroundColor: '#fafafa' }}>
 
-            <Col xs={23} md={5} >
-                <h3><ExperimentFilled style={{ fontSize: '2rem', color: '#009688' }} />Nutrimap</h3>
+            <Col xs={24} className='nutrimapCardCol'>
+                {/* <h3><ExperimentFilled style={{ fontSize: '2rem', color: '#009688' }} />Nutrimap</h3>
                 <p style={{ width: '20rem' }}><span style={{ color: '#009688' }}>Nutrimap</span> provides institutions with a complete
                     nutrient breakdown of individual food items based weight and ingredients. </p>
                 <p style={{ width: '20rem' }}>What's provided:</p>
@@ -44,7 +52,49 @@ const Health = () => {
 
                 <Button onClick={nextStep} type="primary" shape="round" size={'large'}>
                     Next
-                </Button>
+                </Button> */}
+
+                <Card className='card'>
+                    <h2>Nutrimap Registration</h2>
+                    <h3>Tell us about your institution. </h3>
+                    <Form
+                        name="basic"
+                        labelCol={{ span: 8 }}
+                        wrapperCol={{ span: 16 }}
+                        initialValues={{ remember: true }}
+                        onFinish={onFinish}
+                        onFinishFailed={onFinishFailed}
+                        autoComplete="off"
+                    >
+                        <Form.Item
+                            label="Username"
+                            name="username"
+                            rules={[{ required: true, message: 'Please input your username!' }]}
+                        >
+                            <Input />
+                        </Form.Item>
+
+                        <Form.Item
+                            label="Password"
+                            name="password"
+                            rules={[{ required: true, message: 'Please input your password!' }]}
+                        >
+                            <Input.Password />
+                        </Form.Item>
+
+                        <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 8, span: 16 }}>
+                            <Checkbox>Remember me</Checkbox>
+                        </Form.Item>
+
+                        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+                            <Button type="primary" htmlType="submit">
+                                Submit
+                            </Button>
+                        </Form.Item>
+                    </Form>
+                </Card>
+
+
             </Col>
         </Row>
 
