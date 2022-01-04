c548838721c00f41bac7bad0c26db85660f42576
Kenneth  Cunningham <kennethcunningham@Kenneths-MacBook-Air.local>
2022-01-03 22:52:34 -0500

    new commit

--- mainDevFolder/src/design/mainSite/components/Page/Health.tsx
+++ mainDevFolder/src/design/mainSite/components/Page/Health.tsx
@@ -1,42 +1,79 @@
-import { CheckCircleOutlined, ExperimentFilled } from '@ant-design/icons';
-import { Row, Col, Button, Form, Input, Checkbox, Card } from 'antd';
-import { motion } from 'framer-motion';
-import * as React from 'react';
-import { RiHealthBookFill } from 'react-icons/Ri';
+import { CheckCircleOutlined, ExperimentFilled } from '@ant-design/icons'
+import { Row, Col, Button, Form, Input, Checkbox, Card, Select, Cascader } from 'antd'
+import { motion } from 'framer-motion'
+import * as React from 'react'
+import { RiHealthBookFill } from 'react-icons/Ri'
 import { Link, useHistory } from 'react-router-dom'
 
-
-
-
-
 const Health = () => {
+  let history = useHistory()
 
-    let history = useHistory()
+  const pushHome = () => {
+    history.goBack()
+  }
 
-    const pushHome = () => {
-        history.goBack()
+  const nextStep = e => {
+    history.push('/health/nutritionalanalysis')
+  }
 
-    }
+  const onFinish = (values: any) => {
+    console.log('Success:', values)
+  }
 
-    const nextStep = (e) => {
-        history.push('/health/nutritionalanalysis')
-    }
+  const onFinishFailed = (errorInfo: any) => {
+    console.log('Failed:', errorInfo)
+  }
 
-    const onFinish = (values: any) => {
-        console.log('Success:', values);
-    };
+  const options = [
+    {
+      value: 'zhejiang',
+      label: 'Zhejiang',
+      children: [
+        {
+          value: 'hangzhou',
+          label: 'Hangzhou',
+          children: [
+            {
+              value: 'xihu',
+              label: 'West Lake',
+            },
+          ],
+        },
+      ],
+    },
+    {
+      value: 'jiangsu',
+      label: 'Jiangsu',
+      children: [
+        {
+          value: 'nanjing',
+          label: 'Nanjing',
+          children: [
+            {
+              value: 'zhonghuamen',
+              label: 'Zhong Hua Men',
+            },
+          ],
+        },
+      ],
+    },
+  ];
+  
+  function onChange(value) {
+    console.log(value);
+  }
 
-    const onFinishFailed = (errorInfo: any) => {
-        console.log('Failed:', errorInfo);
-    };
-
-
-
-    return (
-        <Row style={{ height: '90vh', display: 'flex', alignItems: 'center',backgroundColor: '#fafafa' }}>
-
-            <Col xs={24} className='nutrimapCardCol'>
-                {/* <h3><ExperimentFilled style={{ fontSize: '2rem', color: '#009688' }} />Nutrimap</h3>
+  return (
+    <Row
+      style={{
+        height: '90vh',
+        display: 'flex',
+        alignItems: 'center',
+        backgroundColor: '#fafafa'
+      }}
+    >
+      <Col xs={24} className='nutrimapCardCol'>
+        {/* <h3><ExperimentFilled style={{ fontSize: '2rem', color: '#009688' }} />Nutrimap</h3>
                 <p style={{ width: '20rem' }}><span style={{ color: '#009688' }}>Nutrimap</span> provides institutions with a complete
                     nutrient breakdown of individual food items based weight and ingredients. </p>
                 <p style={{ width: '20rem' }}>What's provided:</p>
@@ -55,54 +92,72 @@ const Health = () => {
                     Next
                 </Button> */}
 
-                <Card className='card'>
-                    <motion.div>
-                        1
-                    </motion.div>
-                    <h4>Nutrimap Registration</h4>
-                    <h3>Tell us about your institution. </h3>
-                    <Form
-                        name="basic"
-                        labelCol={{ span: 8 }}
-                        wrapperCol={{ span: 16 }}
-                        initialValues={{ remember: true }}
-                        onFinish={onFinish}
-                        onFinishFailed={onFinishFailed}
-                        autoComplete="off"
-                    >
-                        <Form.Item
-                            label="Username"
-                            name="username"
-                            rules={[{ required: true, message: 'Please input your username!' }]}
-                        >
-                            <Input />
-                        </Form.Item>
+        <Card className='card'>
+          <motion.div>1</motion.div>
+          <h4 className='text_1'>Nutrimap Registration</h4>
+          <h3 className='text_2'>Tell us about your institution. </h3>
+          <Form
+            name='basic'
+            wrapperCol={{ span: 24 }}
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete='off'
+            size='large'
+          >
+            <Form.Item
+              name='institution_name'
+              rules={[
+                { required: true, message: 'Please input your username!' }
+              ]}
+            >
+              <Input placeholder='Name' className='input' />
+            </Form.Item>
 
-                        <Form.Item
-                            label="Password"
-                            name="password"
-                            rules={[{ required: true, message: 'Please input your password!' }]}
-                        >
-                            <Input.Password />
-                        </Form.Item>
+            <Form.Item
+              name='institution_email'
+              rules={[
+                { required: true, message: 'Please input your password!' }
+              ]}
+            >
+              <Input placeholder='Email Address' className='input' />
+            </Form.Item>
 
-                        <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 8, span: 16 }}>
-                            <Checkbox>Remember me</Checkbox>
-                        </Form.Item>
+            <Form.Item
+              name='institution_phone'
+              rules={[
+                { required: true, message: 'Please input your password!' }
+              ]}
+            >
+              <Input placeholder='Phone Number' className='input' />
+            </Form.Item>
+            <Form.Item  
+             name='institution_market'
+             rules={[
+               { required: true, message: 'Please input your password!' }
+             ]}
+            >
+            <Cascader className='input' options={options} onChange={onChange} placeholder="Industry" />
 
-                        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-                            <Button type="primary" htmlType="submit">
-                                Submit
-                            </Button>
-                        </Form.Item>
-                    </Form>
-                </Card>
+            </Form.Item>
 
+            <Form.Item >
+              <motion.button
+                className='button'
+                whileHover={{
+                  border: '2px dashed darkgoldenrod',
+                  backgroundColor: 'darkgoldenrod'
+                }}
+                type='submit'
+              >
+                <Link to='/health'>Next</Link>
+              </motion.button>
+            </Form.Item>
+          </Form>
+        </Card>
+      </Col>
+    </Row>
+  )
+}
 
-            </Col>
-        </Row>
-
-    );
-};
-
-export default Health;
+export default Health
